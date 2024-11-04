e55de235518da3f6ee26dc97451dce326a3191dc
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-11-04 11:54:31 -0300

    a

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,10 @@ public class EntregadorController {
     public Entregador obterPorID(@PathVariable Long id) {
         return EntregadorService.obterPorID(id);
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Entregador> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
 
+        EntregadorService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
 }
