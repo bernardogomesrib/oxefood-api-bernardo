14bbcda59ee383b14cdb56e8db2ee5e8f50856a5
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-11-11 10:06:43 -0300

    a

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -17,31 +18,46 @@ import lombok.NoArgsConstructor;
 public class EntregadorRequest {
 
     private Empresa empresa;
+
     private String nome;
     private String cpf;
     private String rg;
-    @JsonFormat(pattern = "dd/MM/yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate dataNascimento;
     private String foneCelular;
     private String foneFixo;
     private Integer qtdEntregasRealizadas;
     private Double valorFrete;
+    private String enderecoRua;
+    private String enderecoNumero;
+    private String enderecoBairro;
+    private String enderecoCidade;
+    private String enderecoCep;
+    private String enderecoUf;
+    private String complemento;
     private Boolean ativo;
 
-   public Entregador build() {
+    public Entregador build() {
 
-       return Entregador.builder()
-       .empresa(empresa)
-       .nome(nome)
-       .cpf(cpf)
-       .rg(rg)
-       .dataNascimento(dataNascimento)
-       .foneCelular(foneCelular)
-       .foneFixo(foneFixo)
-       .qtdEntregasRealizadas(qtdEntregasRealizadas)
-       .valorFrete(valorFrete)
-       .ativo(ativo)
-           .build();
-   }
+        return Entregador.builder()
+                .empresa(empresa)
+                .nome(nome)
+                .cpf(cpf)
+                .rg(rg)
+                .dataNascimento(dataNascimento)
+                .foneCelular(foneCelular)
+                .foneFixo(foneFixo)
+                .qtdEntregasRealizadas(qtdEntregasRealizadas)
+                .valorFrete(valorFrete)
+                .ativo(ativo)
+                .complemento(complemento)
+                .enderecoBairro(enderecoBairro)
+                .enderecoCep(enderecoCep)
+                .enderecoCidade(enderecoCidade)
+                .enderecoNumero(enderecoNumero)
+                .enderecoRua(enderecoRua)
+                .enderecoUf(enderecoUf)
+                .build();
+    }
 
 }
